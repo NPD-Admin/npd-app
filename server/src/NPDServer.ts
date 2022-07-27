a1bd12afbd9a7b2bae044ee1c92ad083f2a8ec8c
Will McVay <mcvay.will@gmail.com>
2022-07-27 00:40:52 -0400

    cors

--- server/src/NPDServer.ts
+++ server/src/NPDServer.ts
@@ -31,8 +31,8 @@ export class NPDServer {
     }));
 
     app.use(function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", "https://www.nonpartisande.org");
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
       next();
     });
 
