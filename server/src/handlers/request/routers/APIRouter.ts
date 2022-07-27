f880b0a7207a6aba4a9939b70ddd0c2d06a1ccbd
Will McVay <mcvay.will@gmail.com>
2022-07-27 16:30:43 -0400

    lookup error handling

--- server/src/handlers/request/routers/APIRouter.ts
+++ server/src/handlers/request/routers/APIRouter.ts
@@ -23,6 +23,7 @@ export class APIRouter {
 
     router.post('/legLookup', async (req: Request, res: Response) => {
       const data = await GeoLookup.findLegislators(req.body.address)
+        .catch(e => res.status(503).json(e));
       if (data instanceof Error) res.json({ error: data.message });
       else res.json(data);
     });
--- server/src/utils/GeoLookup.ts
+++ server/src/utils/GeoLookup.ts
@@ -44,6 +44,8 @@ export type GeoLocation = {
 export class GeoLookup {
   static async findAddress(address: string): Promise<AddressCandidate | Error> {
     const addressResponse = JSON.parse((await HTTPSRequest.httpsGetRequest(GEO_URL+address)).toString()) as AddressResponse;
+    if (!addressResponse) return new Error('Failed to load address response.');
+    if (!addressResponse.candidates) return new Error(`No address candidates found:\n${JSON.stringify(addressResponse, null, 2)}`);
     const candidates = addressResponse.candidates;
     if (!candidates.length || candidates[0].score < 100)
       return new Error(`Address not found.  Closest match:\n${candidates[0].address} (${candidates[0].score}%)`);
@@ -56,6 +58,12 @@ export class GeoLookup {
     if ((response as Error).message) return response as Error;
 
     const candidate = response as AddressCandidate;
+    if (!candidate.spatialReference || !candidate.spatialReference.wkid)
+      return new Error(`Missing spatial references:\n${JSON.stringify(candidate, null, 2)}`);
+    
+    if (!candidate.location || !candidate.location.x || !candidate.location.y)
+      return new Error(`Missing location data:\n${JSON.stringify(candidate, null, 2)}`);
+    
     const geometry = {
       x: candidate.location.x,
       y: candidate.location.y,
@@ -63,18 +71,37 @@ export class GeoLookup {
     };
     const queryString = JSON.stringify(geometry);
     const districtRequests = [0, 1, 2, 3].map(i => HTTPSRequest.httpsGetRequest(DIS_URL.replace(/XX/g, i.toString())+queryString));
-    const districtData = (await Promise.all(districtRequests)).map(response => JSON.parse(response.toString())).map(data => data.features[0].attributes);
+    const districtData = (await Promise.all(districtRequests)).map(response => JSON.parse(response.toString()))
+      .map((data, idx) => {
+        if (!data || !data.features || !data.features.length || !data.features[0].attributes)
+          return new Error(`Missing response data from district query (${idx}):\n${JSON.stringify(geometry, null, 2)}`);
+        return data.features[0].attributes;
+      });
+    
+    if (!districtData) return new Error(`No district data found for queries:\n${JSON.stringify(geometry, null, 2)}`);
+    const error = districtData.find(d => d instanceof Error);
+    if (error) return error;
 
+    if (!districtData.length || !districtData[0].RDED_2012)
+      return new Error(`Unable to load ED data:\n${JSON.stringify(districtData, null, 2)}`);
     const electionDistrict = districtData[0].RDED_2012;
 
-    const legDistricts = [1, 2].map(i => ({
-      district: districtData[i].DISTRICT,
-      name: districtData[i].NAME,
-      party: districtData[i].PARTY,
-      session: districtData[i].LEGSESSION,
-      url: districtData[i].URL.replace(/http/, 'https')
-    }));
+    const legDistricts = [1, 2].map(i => {
+      if (!districtData[i]) return new Error(`Unable to load ${(i === 1 && 'SD') || 'RD'} data for queries:\n${JSON.stringify(geometry, null, 2)}`);
+      if (!districtData[i].URL) return new Error(`Missing URL for ${(i === 1 && 'SD') || 'RD'} data:\n${JSON.stringify(districtData[i], null, 2)}`);
+
+      return {
+        district: districtData[i].DISTRICT,
+        name: districtData[i].NAME,
+        party: districtData[i].PARTY,
+        session: districtData[i].LEGSESSION,
+        url: districtData[i].URL.replace(/http/, 'https')
+      };
+    });
+    const districtError = legDistricts.find(d => d instanceof Error) as Error;
+    if (districtError) return districtError;
     
+    if (!districtData[3]) return new Error(`Missing CD data for queries:\n${JSON.stringify(geometry, null, 2)}`);
     const countyDistrict = {
       district: districtData[3].DISTRICT,
       title: districtData[3].TITLE,
@@ -87,8 +114,8 @@ export class GeoLookup {
     return {
       address: candidate.address,
       ED: electionDistrict,
-      RD: legDistricts[1],
-      SD: legDistricts[0],
+      RD: legDistricts[1] as Legislator,
+      SD: legDistricts[0] as Legislator,
       county: countyDistrict.county,
       CD: countyDistrict
     }
@@ -103,15 +130,19 @@ export class GeoLookup {
 
     const redirectRequests = [districts.RD.url, districts.SD.url].map(url => HTTPSRequest.httpsGetRequest(url));
     const redirectHtmlData = (await Promise.all(redirectRequests).catch(console.error))?.map(html => html.toString());
-    const redirectPaths = redirectHtmlData?.map((data, idx) => hostUrls[idx] + /href="(.+)"/.exec(data)![1]);
+    if (!redirectHtmlData) return districtResponse;
 
-    const redirectAgainRequests = redirectPaths!.map(url => HTTPSRequest.httpsGetRequest(url));
+    const redirectPaths = redirectHtmlData.map((data, idx) => hostUrls[idx] + /href="(.+)"/.exec(data)![1]);
+    const redirectAgainRequests = redirectPaths.map(url => HTTPSRequest.httpsGetRequest(url));
     const redirectAgainHtmlData = (await Promise.all(redirectAgainRequests).catch(console.error))?.map(html => html.toString());
-    const redirectAgainPaths = redirectAgainHtmlData?.map((data, idx) => hostUrls[idx] + /href="(.+)"/.exec(data)![1]);
+    if (!redirectAgainHtmlData) return districtResponse;
 
-    const legRequests = redirectAgainPaths!.map(url => HTTPSRequest.httpsGetRequest(url));
+    const redirectAgainPaths = redirectAgainHtmlData.map((data, idx) => hostUrls[idx] + /href="(.+)"/.exec(data)![1]);
+    const legRequests = redirectAgainPaths.map(url => HTTPSRequest.httpsGetRequest(url));
     const htmlData = (await Promise.all(legRequests).catch(console.error))?.map(html => html.toString());
-    const html = htmlData?.map(data => (load(data)('.info-value > a')[0].children[0] as Text).data);
+    if (!htmlData) return districtResponse;
+
+    const html = htmlData.map(data => (load(data)('.info-value > a')[0].children[0] as Text).data);
 
     if (redirectAgainPaths?.length === 2) {
       (districtResponse as GeoLocation).RD.url = redirectAgainPaths[0];
