45c380adab906c006b2bcafc0a5a078c9a848a68
Will McVay <mcvay.will@gmail.com>
2022-07-28 14:09:26 -0400

    logging

--- scripts/build.js
+++ scripts/build.js
@@ -2,12 +2,15 @@ import { concurrently } from 'concurrently';
 import { resolve } from 'path';
 
 (async () => {
+  console.log('Beginning npm installs...');
   const installResults = await concurrently(['server', 'main', 'widgets'].map(d => ({
     command: 'npm i',
     cwd: resolve(process.cwd(), d)
-  }))).result;
+  }))).result.catch(console.error);
+  console.log('Installs complete, beginning npm builds...');
   await concurrently(installResults.map(d => ({
     command: 'npm run build',
     cwd: d.command.cwd
-  }))).result;
+  }))).result.catch(console.error);
+  console.log('Builds complete.');
 })();
